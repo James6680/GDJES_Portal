90cdabd9b6d078f8d1159d0e7732f1bf58916e28
adrian023 <azfabonan@iskolarngbayan.pup.edu.ph>
2023-10-09 18:09:21 +0800

    new file

---
+++ resources/views/helloworld.blade.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+  @vite(['resources/css/app.css','resources/js/app.js'])
+</head>
+<body>
+  <h1 class="text-3xl font-bold underline bg-green-800">
+    Hello world!
+  </h1>
+</body>
+</html>
