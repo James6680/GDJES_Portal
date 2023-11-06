80b8f6550b1841acc926ab1841df1c81e7bc2888
adrian023 <azfabonan@iskolarngbayan.pup.edu.ph>
2023-11-06 19:00:25 +0800

    fixed the sb point  to other and content generated

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,31 @@ Route::get('layouts.landing', function () {
     return view('layouts.landing');
 });
 
-Route::get('layouts.admin', function () {
+///////////////////////////////////////////////////////ALL ADMIN/////////////////////////////////////
+Route::get('admin.dashboard', function () {
     return view('layouts.admin');
 });
+
+Route::get('admin.announcements', function () {
+    return view('layouts.admin');
+});
+
+Route::get('admin.accounts', function () {
+    return view('layouts.admin');
+});
+
+Route::get('admin.document-request', function () {
+    return view('layouts.admin');
+});
+Route::get('admin.teacher-management', function () {
+    return view('layouts.admin');
+});
+
+Route::get('admin.enrollment-management', function () {
+    return view('layouts.admin');
+});
+
+Route::get('admin.school-information', function () {
+    return view('layouts.admin');
+});
+///////////////////////////////////////////////////////////////////////////////////////////////
