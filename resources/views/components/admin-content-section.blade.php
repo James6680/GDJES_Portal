1347310661e2e470ffda8aef256d70fbaea2b641
lxxSinigang <alexreyeseduc@gmail.com>
2023-11-17 23:40:09 +0800

    start sy modal comment

--- resources/views/components/admin-content-section.blade.php
+++ resources/views/components/admin-content-section.blade.php
@@ -3015,7 +3015,7 @@
         </div>
 
 \
-        <!-- End SY Modal -->
+        <!-- Start SY Modal -->
         <div id="startSYModal" tabindex="-1" class="fixed top-0 left-0 right-0 z-50 hidden p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
           <div class="relative w-full max-w-md max-h-full">
             <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
