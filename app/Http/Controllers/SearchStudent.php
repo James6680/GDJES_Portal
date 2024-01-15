01c908decac7186bd6b4aa6e267c382e0bbf5b8d
adrian023 <azfabonan@iskolarngbayan.pup.edu.ph>
2024-01-15 22:58:42 +0800

    "fixed search student"

--- app/Http/Controllers/SearchStudent.php
+++ app/Http/Controllers/SearchStudent.php
@@ -13,14 +13,19 @@ public function search(Request $request)
     $term = $request->query('term');
     $teacherId = $request->query('teacherId');
 
-    $results = GradeSum::whereHas('class.section', function ($query) use ($teacherId) {
+    $results = GradeSum::join('school_years', 'school_years.id', '=', 'grade_sum.school_year_id')
+    ->where('school_years.active', 1)
+    ->whereHas('class.section', function ($query) use ($teacherId) {
         $query->where('adviser_id', $teacherId);
-    })->whereHas('student', function ($query) use ($term) {
+    })
+    ->whereHas('student', function ($query) use ($term) {
         $query->where('last_name', 'LIKE', "%{$term}%")
             ->orWhere('first_name', 'LIKE', "%{$term}%")
             ->orWhere('middle_name', 'LIKE', "%{$term}%");
-    })->where('school_year_id', '=', 1)
-    ->with('student', 'class.subject')->get();
+    })
+    ->with('student', 'class.subject')
+    ->get();
+
 
 return response()->json($results);
 
