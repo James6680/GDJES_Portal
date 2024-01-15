2aa52308d4f65b7ee302d42018750f4b385395f6
beyzepgul <karenjtiro@iskolarngbayan.pup.edu.ph>
2024-01-15 17:51:52 +0800

    grades

--- resources/views/scripts_with_ajax.blade.php
+++ resources/views/scripts_with_ajax.blade.php
@@ -777,3 +777,38 @@
         });
     });
 </script>
+
+
+<!-- JavaScript to handle modal visibility -->
+<script>
+    document.getElementById('openModale').addEventListener('click', function() {
+        document.getElementById('myModale').classList.remove('hidden');
+    });
+
+    document.getElementById('GWAconfirmNo').addEventListener('click', function() {
+        document.getElementById('myModale').classList.add('hidden');
+    });
+
+    document.getElementById('GWAconfirmYes').addEventListener('click', function() {
+      var schoolYearId = this.getAttribute('data-school-year-id');
+      var sectionId = this.getAttribute('data-section-id');
+      var gradeLevelId = this.getAttribute('data-grade-level-id');
+      
+      // Make an AJAX request to update the 'posted' column in the gwas table
+      fetch('/update-posted', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+              'X-CSRF-TOKEN': '{{ csrf_token() }}'
+          },
+          body: JSON.stringify({
+              schoolYearId: schoolYearId,
+              sectionId: sectionId,
+              gradeLevelId: gradeLevelId
+          })
+        }).then(response => response.json())
+        .then(data => {
+        document.getElementById('myModale').classList.add('hidden');
+      });
+    });
+  </script>
