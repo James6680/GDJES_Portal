91760d3041e16f03b095be4676d1cf9e5986cf97
adrian023 <azfabonan@iskolarngbayan.pup.edu.ph>
2024-01-15 12:56:42 +0800

    ""

--- app/View/Components/FacultyContentSection.php
+++ app/View/Components/FacultyContentSection.php
@@ -117,28 +117,33 @@ class FacultyContentSection extends Component
 
         
 
+        if($class_idValue != null){
+                // Fetch the required data from the GradeSum table and related tables
+                $gradeSums = GradeSum::with('student', 'class', 'schoolYear')
+                ->where('class_id', $class_idValue) // Filter by selected class_id
+                ->get();    
 
-        // Fetch the required data from the GradeSum table and related tables
-        $gradeSums = GradeSum::with('student', 'class', 'schoolYear')
-        ->where('class_id', $class_idValue) // Filter by selected class_id
-        ->get();
-
-        
-        // Transform the $gradeSums for use in the view
+                        // Transform the $gradeSums for use in the view
         $gradeSumData = $gradeSums->map(function ($gradeSum) {
-        return [
-            'id' => $gradeSum->id,
-            'student_name' => $gradeSum->student->last_name . ', ' . $gradeSum->student->first_name . ' ' . $gradeSum->student->middle_name,
-            'grade_q1' => $gradeSum->grade_q1,
-            'grade_q2' => $gradeSum->grade_q2,
-            'grade_q3' => $gradeSum->grade_q3,
-            'grade_q4' => $gradeSum->grade_q4,
-            'average' => $gradeSum->average,
-            'descriptor' =>$gradeSum->descriptor,
-            'remarks' => $gradeSum->remarks,
-            
-        ];
-        });
+            return [
+                'id' => $gradeSum->id,
+                'student_name' => $gradeSum->student->last_name . ', ' . $gradeSum->student->first_name . ' ' . $gradeSum->student->middle_name,
+                'grade_q1' => $gradeSum->grade_q1,
+                'grade_q2' => $gradeSum->grade_q2,
+                'grade_q3' => $gradeSum->grade_q3,
+                'grade_q4' => $gradeSum->grade_q4,
+                'average' => $gradeSum->average,
+                'descriptor' =>$gradeSum->descriptor,
+                'remarks' => $gradeSum->remarks,
+                
+            ];
+            });
+
+        }else{
+            $gradeSums = new GradeSum;
+            $gradeSumData = [];
+        }
+
 
         // Get the teacher ID of the currently logged-in teacher
         $teacherId = Auth::guard('teachers')->user()->id;
